68222bff4db24313cc72b5b49bcdacda622e9624
mehmetfurkanaydin <aydin.mehmetfurkan@gmail.com>
2024-12-08 12:09:24 +0300

    finalize announcements component

--- src/app/pages/dashboard/announcements/announcements.component.ts
+++ src/app/pages/dashboard/announcements/announcements.component.ts
@@ -1,250 +1,227 @@
-import { CommonModule, DatePipe } from '@angular/common';
-import { ChangeDetectorRef, Component, Inject, ViewChild } from '@angular/core';
-import {
-  FormBuilder,
-  FormGroup,
-  FormsModule,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import {
-  MAT_DIALOG_DATA,
-  MatDialog,
-  MatDialogActions,
-  MatDialogClose,
-  MatDialogContent,
-  MatDialogRef,
-  MatDialogTitle,
-} from '@angular/material/dialog';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import {
-  MatPaginator,
-  MatPaginatorIntl,
-  MatPaginatorModule,
-} from '@angular/material/paginator';
-import { MatSort, MatSortModule } from '@angular/material/sort';
+import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-import { BreakpointObserver } from '@angular/cdk/layout';
-import { sortBy } from 'lodash';
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 import {
   collection,
+  doc,
   getDocs,
   getFirestore,
-  doc,
-  updateDoc,
-  setDoc,
   addDoc,
+  setDoc,
   deleteDoc,
+  query,
+  where,
+  Query,
+  DocumentData,
 } from '@angular/fire/firestore';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatSelectModule } from '@angular/material/select';
+import { CommonModule, DatePipe, formatDate } from '@angular/common';
+import { AuthService } from '../../../auth.service';
+import { AnnouncementDialogComponent } from './announcement-dialog.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 export interface AnnouncementData {
-  id: string;
+  id?: string;
   title: string;
   description: string;
   date: string;
+  audienceType: string;
+  city?: string;
+  district?: string;
 }
 
 @Component({
   selector: 'app-announcements',
   standalone: true,
   imports: [
-    MatFormFieldModule,
-    MatInputModule,
+    CommonModule,
     MatTableModule,
     MatSortModule,
     MatPaginatorModule,
     MatButtonModule,
-    FormsModule,
-    CommonModule,
     MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
   ],
   providers: [DatePipe],
   templateUrl: './announcements.component.html',
-  styleUrl: './announcements.component.scss',
+  styleUrls: ['./announcements.component.scss'],
 })
-export class AnnouncementsComponent {
+export class AnnouncementsComponent implements OnInit {
   displayedColumns: string[] = [
     'id',
     'title',
-    'description',
+    'audienceType',
     'date',
     'actions',
   ];
-  dataSource: MatTableDataSource<AnnouncementData> = new MatTableDataSource();
-  isMobile = false;
+  dataSource = new MatTableDataSource<AnnouncementData>();
 
-  @ViewChild(MatPaginator) paginator: MatPaginator = new MatPaginator(
-    new MatPaginatorIntl(),
-    ChangeDetectorRef.prototype
-  );
-  @ViewChild(MatSort) sort: MatSort = new MatSort();
+  constructor(private dialog: MatDialog, private authService: AuthService) {}
 
-  constructor(
-    public dialog: MatDialog,
-    private datePipe: DatePipe,
-    breakpointObserver: BreakpointObserver
-  ) {
-    this.getAnnouncements();
-    breakpointObserver.observe(['(max-width: 600px)']).subscribe((result) => {
-      this.displayedColumns = result.matches
-        ? ['title', 'description', 'date', 'actions']
-        : ['id', 'title', 'description', 'date', 'actions'];
-    });
-  }
-
-  async updateEmail(email: string, id: string) {
-    const userRef = doc(getFirestore(), 'users', id);
-    await updateDoc(userRef, {
-      email,
-    });
+  ngOnInit(): void {
     this.getAnnouncements();
   }
 
-  formatDate(date: string): string {
-    return this.datePipe.transform(date, 'dd/MM/yyyy')!;
-  }
-
-  async deleteAnnouncement(event: any, row: AnnouncementData) {
-    event.stopPropagation();
-    if (confirm('Bu duyuruyu silmek üzeresiniz, emin misiniz?')) {
-      await deleteDoc(doc(getFirestore(), 'announcements', row.id));
-      this.getAnnouncements();
-    }
-  }
-
-  createEmptyAnnouncement(): AnnouncementData {
-    return {
-      id: '',
-      title: '',
-      description: '',
-      date: '',
-    };
-  }
-
-  async createAnnouncement(data: AnnouncementData) {
-    const { id, ...rest } = data;
-    await addDoc(collection(getFirestore(), 'announcements'), rest);
-    this.getAnnouncements();
-  }
-
-  async updateAnnouncement(data: AnnouncementData) {
-    await setDoc(doc(getFirestore(), 'announcements', data.id), data);
-    this.getAnnouncements();
-  }
-
-  openDialog(row: AnnouncementData): void {
-    const dialogRef = this.dialog.open(AnnouncementDialogComponent, {
-      data: row,
-    });
-
-    dialogRef.afterClosed().subscribe((result) => {
-      if (result) {
-        if (result.id) {
-          this.updateAnnouncement(result);
-        } else {
-          this.createAnnouncement(result);
-        }
-      }
-    });
-  }
-
-  onAddNewAnnouncement = () => {
-    const emptyAnnouncement = this.createEmptyAnnouncement();
-    this.openDialog(emptyAnnouncement);
-  };
-
-  async getAnnouncements() {
-    const announcements: AnnouncementData[] = [];
-    const querySnapshot = await getDocs(
-      collection(getFirestore(), 'announcements')
-    );
-    querySnapshot.forEach((doc) => {
-      announcements.push({
-        id: doc.id,
-        title: doc.data()['title'],
-        description: doc.data()['description'],
-        date: doc.data()['date'],
-      });
-    });
-    this.dataSource = new MatTableDataSource(sortBy(announcements, 'date'));
-  }
-
-  onChangeEvent(event: any, row: AnnouncementData) {
-    event.stopPropagation();
-    this.openDialog(row);
-  }
-
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
-  }
-
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+  applyFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value
+      .trim()
+      .toLowerCase();
+    this.dataSource.filter = filterValue;
 
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
   }
-}
 
-@Component({
-  selector: 'announcement-dialog',
-  templateUrl: 'announcement-dialog.html',
-  standalone: true,
-  imports: [
-    MatFormFieldModule,
-    MatInputModule,
-    FormsModule,
-    MatButtonModule,
-    MatDialogTitle,
-    MatDialogContent,
-    MatDialogActions,
-    MatDialogClose,
-    MatDatepickerModule,
-    ReactiveFormsModule,
-    MatSelectModule,
-  ],
-  styles: [
-    `
-      form {
-        display: flex;
-        flex-direction: column;
+  async getAnnouncements(): Promise<void> {
+    const announcements: AnnouncementData[] = [];
+    const firestore = getFirestore();
 
-        @media (min-width: 600px) {
-          min-width: 800px;
-        }
+    const currentUserRole = this.authService.getUserRole();
+    const currentUserCity = this.authService.getUserCity();
+    const currentUserDistrict = this.authService.getUserDistrict();
+
+    let queryRef: Query<DocumentData>;
+
+    if (currentUserRole === 'headquarters') {
+      queryRef = collection(firestore, 'announcements') as Query<DocumentData>;
+      const querySnapshot = await getDocs(queryRef);
+
+      querySnapshot.forEach((doc) => {
+        announcements.push({
+          id: doc.id,
+          ...doc.data(),
+        } as AnnouncementData);
+      });
+    } else if (currentUserRole === 'city' || currentUserRole === 'district') {
+      const everyoneQuery = query(
+        collection(firestore, 'announcements'),
+        where('audienceType', '==', 'everyone')
+      );
+
+      const roleSpecificQuery =
+        currentUserRole === 'city'
+          ? query(
+              collection(firestore, 'announcements'),
+              where('city', '==', currentUserCity),
+              where('audienceType', 'in', ['city', 'district'])
+            )
+          : query(
+              collection(firestore, 'announcements'),
+              where('district', '==', currentUserDistrict),
+              where('audienceType', '==', 'district')
+            );
+
+      const [everyoneSnapshot, roleSpecificSnapshot] = await Promise.all([
+        getDocs(everyoneQuery),
+        getDocs(roleSpecificQuery),
+      ]);
+
+      everyoneSnapshot.forEach((doc) => {
+        announcements.push({ id: doc.id, ...doc.data() } as AnnouncementData);
+      });
+
+      roleSpecificSnapshot.forEach((doc) => {
+        announcements.push({ id: doc.id, ...doc.data() } as AnnouncementData);
+      });
+    }
+
+    this.dataSource = new MatTableDataSource(announcements);
+  }
+
+  async onAddNewAnnouncement(): Promise<void> {
+    const dialogRef = this.dialog.open(AnnouncementDialogComponent, {
+      width: '500px',
+      data: {
+        announcement: {
+          id: null,
+          title: '',
+          description: '',
+          date: '',
+          audienceType: '',
+        },
+        readonly: false,
+      },
+    });
+
+    dialogRef.afterClosed().subscribe(async (result) => {
+      if (result) {
+        await this.createAnnouncement(result);
       }
-    `,
-  ],
-})
-export class AnnouncementDialogComponent {
-  announcementForm: FormGroup;
-
-  constructor(
-    private fb: FormBuilder,
-    public dialogRef: MatDialogRef<AnnouncementDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: AnnouncementData
-  ) {
-    this.announcementForm = this.fb.group({
-      id: [data.id],
-      title: [data.title, Validators.required],
-      description: [data.description, Validators.required],
-      date: [data.date ? new Date(data.date) : '', Validators.required],
     });
   }
 
-  onSubmit(): void {
-    if (this.announcementForm.valid) {
-      const formValue = this.announcementForm.value;
-      formValue.date = formValue.date.toISOString();
-      this.dialogRef.close(this.announcementForm.value);
+  async onEditAnnouncement(event: Event, row: AnnouncementData): Promise<void> {
+    event.stopPropagation();
+    const dialogRef = this.dialog.open(AnnouncementDialogComponent, {
+      width: '500px',
+      data: { announcement: row, readonly: false },
+    });
+
+    dialogRef.afterClosed().subscribe(async (result) => {
+      if (result) {
+        await this.updateAnnouncement(result);
+      }
+    });
+  }
+
+  async deleteAnnouncement(event: Event, row: AnnouncementData): Promise<void> {
+    event.stopPropagation();
+
+    if (!row.id) {
+      console.error(
+        'Announcement ID is undefined. Cannot delete the announcement.'
+      );
+      return;
     }
+
+    if (confirm('Bu duyuruyu silmek istediğinizden emin misiniz?')) {
+      try {
+        const firestore = getFirestore();
+        const announcementRef = doc(firestore, 'announcements', row.id);
+        await deleteDoc(announcementRef);
+        this.getAnnouncements();
+      } catch (error) {
+        console.error('Error deleting announcement:', error);
+      }
+    }
+  }
+
+  async createAnnouncement(data: AnnouncementData): Promise<void> {
+    const { id, ...rest } = data;
+    await addDoc(collection(getFirestore(), 'announcements'), rest);
+    this.getAnnouncements();
+  }
+
+  async updateAnnouncement(data: AnnouncementData): Promise<void> {
+    await setDoc(doc(getFirestore(), 'announcements', data.id!), data);
+    this.getAnnouncements();
+  }
+
+  formatDate(date: string): string {
+    return formatDate(date, 'dd.MM.yyyy', 'en-US');
+  }
+
+  getAudienceText(audienceType: string): string {
+    const audienceMap: { [key: string]: string } = {
+      everyone: 'Herkes',
+      headquarters: 'Genel Merkez',
+      city: 'Şehir',
+      district: 'İlçe',
+    };
+    return audienceMap[audienceType] || 'Bilinmeyen';
+  }
+
+  openDisplayDialog(announcementData: AnnouncementData): void {
+    this.dialog.open(AnnouncementDialogComponent, {
+      width: '500px',
+      data: { announcement: announcementData, readonly: true },
+    });
   }
 }
