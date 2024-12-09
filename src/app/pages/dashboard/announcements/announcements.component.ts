38218e5b709d7eba7c84a659ca6683eef30dfb6f
mehmetfurkanaydin <aydin.mehmetfurkan@gmail.com>
2024-12-10 01:51:18 +0300

    implement executive-committee

--- src/app/pages/dashboard/announcements/announcements.component.ts
+++ src/app/pages/dashboard/announcements/announcements.component.ts
@@ -23,7 +23,6 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 export interface AnnouncementData {
   id?: string;
